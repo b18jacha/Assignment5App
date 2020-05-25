e65626d84f84a7ec4282d499f511b02e34012a66
Jacob Hafdell <b18jacha@student.his.se>
2020-05-25 14:37:26 +0200

    - Added Internet Access. - Added a Snackbar that displays text when pushing a button. - Added mytextview.xml

--- app/src/main/java/com/example/assignment5app/MainActivity.java
+++ app/src/main/java/com/example/assignment5app/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.assignment5app;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView myListView = findViewById(R.id.myListView);
+
+        Button myButton = findViewById(R.id.targetButton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String targetGroup = "This is just a test text";
+                Snackbar.make(myListView, targetGroup, Snackbar.LENGTH_LONG).show();
+            }
+        });
+
     }
+
 }
